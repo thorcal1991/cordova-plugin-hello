ab6e606fe3fa6042913fad82ff88490961ccc7ff
thorcal1991 <38915989+thorcal1991@users.noreply.github.com>
2020-07-17 13:58:34 +0200

    Update CheckMockLocation.java

--- src/android/CheckMockLocation.java
+++ src/android/CheckMockLocation.java
@@ -3,6 +3,7 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.provider.Settings;
 
 public class CheckMockLocation extends CordovaPlugin {
 
@@ -10,9 +11,17 @@ public class CheckMockLocation extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
+            
+            int adb = Settings.Secure.getInt(this.getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED , 0);
+            
+            if(adb == 0) {
+                callbackContext.success("punch");
 
-            callbackContext.success("MOCK LOCATIONNNN");
+            } else {
+                callbackContext.success("izzabbab");
+            }
 
+            
             return true;
 
         } else {
